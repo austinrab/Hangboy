2405ee78c7474768e424e4be7b4beca13d7160b2
Austin Rabaza <austinrabaza@gmail.com>
2020-11-27 21:39:40 +0000

    Put buttons on the sceen

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ WIDTH, HEIGHT = 800,500
 win = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Hangboy Game!")
 
+# the button variables 
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH-(RADIUS*2+GAP)*13)/2)
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i%13))
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x,y])
+
+
+
+
+
 #loading images 
 images = []
 for i in range(7):
@@ -18,6 +33,7 @@ hangman_status = 0
 
 #colors
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 
 
@@ -29,12 +45,25 @@ FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+def draw():
+    win.fill(WHITE)
+
+    #draw buttons
+
+    for letter in letters:
+        x,y = letter
+        pygame.draw.circle(win,BLACK, (x,y),RADIUS,3)
+
+
+    win.blit(images[hangman_status],(150,100))
+    pygame.display.update()
+
+
+
 while run:
     clock.tick(FPS)
 
-    win.fill(WHITE)
-    win.blit(images[hangman_status],(150,100))
-    pygame.display.update()
+    draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
