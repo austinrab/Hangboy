b2b4d5171f655825cdec35a8fbec4f25fdccd343
Austin Rabaza <austinrabaza@gmail.com>
2020-11-27 21:23:57 +0000

    images working

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import pygame
-import os
+
+#set up display
 
 pygame.init()
 WIDTH, HEIGHT = 800,500
-pygame.display.set_mode((WIDTH,HEIGHT))
+win = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Hangboy Game!")
 
-FPS = 
+#loading images 
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+
+#game variables
+hangman_status = 0
+
+#colors
+WHITE = (255,255,255)
+
+
+
+
+
+
+#setting up gameloop
+FPS = 60
+clock = pygame.time.Clock()
+run = True
+
+while run:
+    clock.tick(FPS)
+
+    win.fill(WHITE)
+    win.blit(images[hangman_status],(150,100))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+
+            
+pygame.quit()
+
